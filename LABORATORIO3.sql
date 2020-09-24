8647ae3de15e43907bb869c145868d15160f3eaa
DiegoGonzalez2807 <59978048+DiegoGonzalez2807@users.noreply.github.com>
2020-09-23 20:06:15 -0500

    Update LABORATORIO3.sql

--- LABORATORIO3.sql
+++ LABORATORIO3.sql
@@ -1,9 +1,105 @@
-CREATE TABLE  Adultos(cedula NUMBER(10)NOT NULL,correo VARCHAR(50),telefonos NUMBER(10));
-CREATE TABLE Personas(Codigo NUMBER(10) NOT NULL,nombre VARCHAR(15),genero VARCHAR(9),talla NUMBER(2),nacimiento DATE);
-CREATE TABLE Opinion(Numero NUMBER(5) NOT NULL, Fecha DATE, Opinion VARCHAR(250), Codigo NUMBER(10) NOT NULL, Codigo_bien NUMBER(10) NOT NULL);
-CREATE TABLE OpinionesGrupales(Razon VARCHAR(250), Estrellas NUMBER(1) NOT NULL);
-CREATE TABLE Familias(Numero NUMBER(3) NOT NULL, Codigo NUMBER(10) NOT NULL, Nombre VARCHAR(20));
-CREATE TABLE Localidades(Nombre VARCHAR(20)NOT NULL, Prioridad VARCHAR(5), Departamento VARCHAR(10) NOT NULL);
-CREATE TABLE Bienes(Codigo NUMBER(10) NOT NULL, Nombre VARCHAR(20), Tipo VARCHAR(10), Medida NUMBER(3), Unitario NUMBER(1), Retirado VARCHAR(2)NOT NULL, Numero_familia NUMBER(3) NOT NULL);
-CREATE TABLE Reemplazo(Codigo1 NUMBER(10) NOT NULL, Codigo2 NUMBER(10) NOT NULL);
-CREATE TABLE Asignaciones(Numero NUMBER(10) NOT NULL,fecha DATE,aceptado VARCHAR(2),numero_familia NUMBER(3) NOT NULL);
+CREATE TABLE adulto (
+    codigo      NUMBER(7) NOT NULL,
+    cedula      NUMBER(12) NOT NULL,
+    correo      VARCHAR(40) NOT NULL,
+    telefono1   NUMBER(12) NOT NULL,
+    telefono2   NUMBER(12)
+);
+
+CREATE TABLE telefono (
+    cedula    NUMBER(12) NOT NULL,
+    telfono   NUMBER(12) NOT NULL
+);
+
+CREATE TABLE persona (
+    codigo       NUMBER(7) NOT NULL,
+    nombre       VARCHAR(50) NOT NULL,
+    genero       VARCHAR(1) NOT NULL,
+    talla        VARCHAR(3) NOT NULL,
+    nacimiento   DATE NOT NULL
+);
+
+CREATE TABLE opinion (
+    numero          NUMBER(5) NOT NULL,
+    fecha           DATE,
+    opinion         VARCHAR(1) NOT NULL,
+    justificacion   VARCHAR(20) NOT NULL,
+    codigo          NUMBER(7) NOT NULL,
+    codigo_bien     VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE opiniongrupal (
+    razon           VARCHAR(200),
+    estrellas       NUMBER(1) NOT NULL,
+    numero          NUMBER(5) NOT NULL,
+    fecha           DATE NOT NULL,
+    opinion         VARCHAR(1) NOT NULL,
+    justificacion   VARCHAR(20) NOT NULL,
+    codigo          NUMBER(7) NOT NULL,
+    codigo_bien     VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE familia (
+    numero   NUMBER(5) NOT NULL,
+    codigo   NUMBER(7) NOT NULL,
+    nombre   VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE localidad (
+    nombre         VARCHAR(15) NOT NULL,
+    prioridad      NUMBER(1) NOT NULL,
+    departamento   VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE remplazo (
+    codigo    VARCHAR(5) NOT NULL,
+    codigo1   VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE alojamiento (
+    nombre     VARCHAR(15) NOT NULL,
+    personas   NUMBER(4) NOT NULL,
+    inicio     DATE NOT NULL,
+    fin        DATE,
+    orden      NUMBER(4) NOT NULL
+);
+
+CREATE TABLE vestuario (
+    cantidad   NUMBER(4) NOT NULL,
+    talla      VARCHAR(3) NOT NULL,
+    orden      NUMBER(4) NOT NULL
+);
+
+CREATE TABLE generico (
+    cantidad   NUMBER(4) NOT NULL,
+    orden      NUMBER(4) NOT NULL
+);
+
+CREATE TABLE perecedero (
+    cantidad      NUMBER(4) NOT NULL,
+    vencimiento   DATE NOT NULL,
+    orden         NUMBER(4) NOT NULL
+);
+
+CREATE TABLE detalle (
+    orden    NUMBER(4) NOT NULL,
+    numero   NUMBER(9) NOT NULL,
+    codigo   VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE bien (
+    codigo     VARCHAR(5) NOT NULL,
+    nombre     VARCHAR(30) NOT NULL,
+    tipo       VARCHAR(1) NOT NULL,
+    medida     VARCHAR(2) NOT NULL,
+    unitario   NUMBER(5) NOT NULL,
+    retirado   NUMBER(1) NOT NULL,
+    numero     NUMBER(5) NOT NULL
+);
+
+CREATE TABLE asignacion (
+    numero          NUMBER(9) NOT NULL,
+    fecha           DATE NOT NULL,
+    aceptado        NUMBER(1),
+    numerofamilia   NUMBER(5) NOT NULL
+);
