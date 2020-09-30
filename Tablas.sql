c33d037f441c739a8356bf34d0bd077163cc4ce7
DiegoGonzalez2807 <59978048+DiegoGonzalez2807@users.noreply.github.com>
2020-09-30 00:26:44 -0500

    Update Tablas.sql

--- Tablas.sql
+++ Tablas.sql
@@ -95,3 +95,28 @@ CREATE TABLE estado (
     calidad           VARCHAR(1) NOT NULL,
     observaciones     VARCHAR(250)
 );
+
+/*PRIMARY KEYS*/
+ALTER TABLE empresa ADD CONSTRAINT pk_empresa_codigo PRIMARY KEY ( codigo );
+
+ALTER TABLE persona_juridica ADD CONSTRAINT pk_juridica_codigo PRIMARY KEY ( codigo );
+
+ALTER TABLE proveedor ADD CONSTRAINT pk_proveedor_codigo PRIMARY KEY ( codigo );
+
+ALTER TABLE bodega ADD CONSTRAINT pk_bodega_nombre PRIMARY KEY ( nombre_bodega );
+
+ALTER TABLE inventario ADD CONSTRAINT pk_inventario_numero PRIMARY KEY ( numero_inventario );
+
+ALTER TABLE dimensiones ADD CONSTRAINT pk_dimension_numero PRIMARY KEY ( numero_pieza );
+
+ALTER TABLE pieza_extraccion_petrolera ADD CONSTRAINT pk_numero_extraccion PRIMARY KEY ( numero_serie );
+
+ALTER TABLE pedido_pieza ADD CONSTRAINT pk_numero_pedido PRIMARY KEY ( numero_pedido );
+
+ALTER TABLE permiso ADD CONSTRAINT pk_numero_permiso PRIMARY KEY ( numero_permiso );
+
+ALTER TABLE empleado ADD CONSTRAINT pk_empleado_id PRIMARY KEY ( id );
+
+ALTER TABLE experto ADD CONSTRAINT pk_experto_id PRIMARY KEY ( id );
+
+ALTER TABLE estado ADD CONSTRAINT pk_estado_revision PRIMARY KEY ( numero_revision );
