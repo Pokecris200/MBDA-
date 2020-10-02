4fe8921ed8b3c235be6fc5fed399a73179580bb1
DiegoGonzalez2807 <59978048+DiegoGonzalez2807@users.noreply.github.com>
2020-10-01 20:31:15 -0500

    Update Tablas.sql

--- Tablas.sql
+++ Tablas.sql
@@ -32,35 +32,42 @@ CREATE TABLE bodega (
 );
 
 CREATE TABLE inventario (
-    numero_inventario       NUMBER(3) NOT NULL,
-    bodegas_automatizadas   NUMBER(3) NOT NULL,
+    nombre_bodega           VARCHAR(20) NOT NULL,   
     numero_serie_pieza      NUMBER(6) NOT NULL,
-    disponibilidad          VARCHAR(13) NOT NULL,
-    nombre_bodega           VARCHAR(20) NOT NULL
+    ID_inventario           VARCHAR(6) NOT NULL,
+    disponibilidad          VARCHAR(13) NOT NULL
 );
 
 CREATE TABLE empleado (
     nombre                 VARCHAR(20) NOT NULL,
     apellido               VARCHAR(20) NOT NULL,
     id                     NUMBER(7) NOT NULL,
-    correo                 VARCHAR(15),
-    numero_telefonico      NUMBER(10),
-    departamento_trabajo   VARCHAR(20) NOT NULL
+    correo                 VARCHAR(15) NOT NULL,
+    numero_telefonico      NUMBER(10) NOT NULL,
+    departamento_trabajo   VARCHAR(20) NOT NULL,
+    cedula                 NUMBER(10) NOT NULL
 );
 
 CREATE TABLE experto (
     id                         NUMBER(7) NOT NULL,
-    departamento_experiencia   VARCHAR(20) NOT NULL,
-    cedula                     NUMBER(10) NOT NULL
+    departamento_experiencia   VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE estado (
+    numero_revision   NUMBER(7) NOT NULL,
+    revisado_por      NUMBER(7) NOT NULL,
+    numero_pieza      NUMBER(6) NOT NULL,
+    calidad           VARCHAR(1) NOT NULL,
+    observaciones     VARCHAR(250)
+);
+
 
 CREATE TABLE permiso (
     numero_permiso    NUMBER(7) NOT NULL,
     id_autor          NUMBER(7) NOT NULL,
     numero_pieza      NUMBER(6) NOT NULL,
-    cantidad_piezas   NUMBER(5) NOT NULL,
     detalle           VARCHAR(250) NOT NULL,
-    observaciones     VARCHAR(100) NOT NULL,
+    observaciones     VARCHAR(100),
     estado            VARCHAR(8) NOT NULL,
     pedido            NUMBER(7)
 );
@@ -70,30 +77,16 @@ CREATE TABLE pedido_pieza (
     id_autor               NUMBER(7) NOT NULL,
     departamento_trabajo   VARCHAR(20) NOT NULL,
     cantidad_piezas        NUMBER(5) NOT NULL,
-    codigo_proveedor       VARCHAR(8) NOT NULL
+    bodega_reclamo         VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE pieza_extraccion_petrolera (
     numero_serie   NUMBER(6) NOT NULL,
     tipo           VARCHAR(15) NOT NULL,
+    dimensiones    VARCHAR(10) NOT NULL,
     estado         VARCHAR(8) NOT NULL
 );
 
-CREATE TABLE dimensiones (
-    numero_pieza   NUMBER(6) NOT NULL,
-    alto           NUMBER(3) NOT NULL,
-    largo          NUMBER(3) NOT NULL,
-    ancho          NUMBER(3) NOT NULL
-);
-
-CREATE TABLE estado (
-    numero_revision   NUMBER(7) NOT NULL,
-    revisado_por      NUMBER(7) NOT NULL,
-    numero_pieza      NUMBER(6) NOT NULL,
-    estado            VARCHAR(250) NOT NULL,
-    calidad           VARCHAR(1) NOT NULL,
-    observaciones     VARCHAR(250)
-);
 
 /*PRIMARY KEYS*/
 ALTER TABLE empresa ADD CONSTRAINT pk_empresa_codigo PRIMARY KEY ( codigo );
